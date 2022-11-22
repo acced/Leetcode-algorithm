741cd02b4a75cea0adb6a492a5811b156b1253ab
Jie <1415351793@qq.com>
2022-11-22 19:39:06 +0800

    Leetcode

--- 27.remove-element.cs
+++ 27.remove-element.cs
@@ -32,7 +32,7 @@ public class Solution {
             
          }
          */
-              int index = 0;
+        int index = 0;
         for(int i = 0; i < nums.Length; i++){
             if(nums[i] != val){
                 nums[index++] = nums[i];
