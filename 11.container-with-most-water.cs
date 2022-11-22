0c71f84677e0286294776c10b72149e2b03fa67b
Jie <1415351793@qq.com>
2022-11-22 22:20:58 +0800

    first commit

---
+++ 11.container-with-most-water.cs
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=11 lang=csharp
+ *
+ * [11] Container With Most Water
+ */
+
+// @lc code=start
+public class Solution11 {
+    public int MaxArea(int[] height) {
+        int start=0,end=height.Length-1,max=0;
+        while(start<end){
+            int width=end-start;
+            int high=height[start]<height[end]?height[start]:high=height[end];
+            if(height[start]<height[end]){
+                 
+                start++;
+            }
+            else {
+                end--;
+            }
+            if(width* high>max) max=width* high;
+        }
+        return max;
+        
+    }
+}
+// @lc code=end
+
