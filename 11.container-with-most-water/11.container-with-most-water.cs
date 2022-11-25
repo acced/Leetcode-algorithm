fd30c4b0716e280d2553b8177fda85b3f1bd3a9b
Jie <1415351793@qq.com>
2022-11-25 17:26:50 +0800

    11.container-with-most-water

--- 11.container-with-most-water/11.container-with-most-water.cs
+++ 11.container-with-most-water/11.container-with-most-water.cs
@@ -5,12 +5,12 @@
  */
 
 // @lc code=start
-public class Solution11 {
+public class Solution {
     public int MaxArea(int[] height) {
         int start=0,end=height.Length-1,max=0;
         while(start<end){
             int width=end-start;
-            int high=height[start]<height[end]?height[start]:high=height[end];
+            int high=height[start]<height[end]?height[start]:height[end];
             if(height[start]<height[end]){
                  
                 start++;
